f78358efa484acd8204395ab0b087a2e5312a6f1
fritzmg <fmg@inspiredminds.at>
2021-08-15 17:40:35 +0100

    refactor ContentTemplateManager

--- src/ContentTemplateManager.php
+++ src/ContentTemplateManager.php
@@ -22,6 +22,7 @@ use InspiredMinds\ContaoContentTemplates\Model\ContentTemplateModel;
 class ContentTemplateManager
 {
     private $framework;
+    private $mappedElements = [];
 
     public function __construct(ContaoFramework $framework)
     {
@@ -46,75 +47,113 @@ class ContentTemplateManager
         }
 
         foreach ($templateArticles as $templateArticle) {
-            $targetArticle = ArticleModel::findOneBy(['pid = ?', 'content_template_source = ?'], [$pageId, $templateArticle->id]);
-
-            if (null === $targetArticle) {
-                $targetArticle = ArticleModel::findOneBy(['alias = ?', 'pid = ?', 'inColumn = ?'], [$templateArticle->alias, $pageId, $templateArticle->inColumn]);
-
-                if (null !== $targetArticle) {
-                    $targetArticle->content_template_source = $templateArticle->id;
-                    $targetArticle->save();
-                }
-            }
-
-            if (null === $targetArticle) {
-                $targetArticle = new ArticleModel();
-                $row = $templateArticle->row();
-                unset($row[ArticleModel::getPk()]);
-                $targetArticle->setRow($row);
-                $targetArticle->pid = $pageId;
-                $targetArticle->save();
-            }
+            $targetArticle = $this->getTargetArticle($pageId, $templateArticle);
 
             // TODO: update settings
 
-            $templateElements = ContentModel::findBy(['pid = ?', 'ptable = ?'], [$templateArticle->id, $templateArticle->getTable()], ['order' => 'sorting ASC']);
+            $this->updateContentElements($templateArticle, $targetArticle);
+        }
+    }
 
-            if (null === $templateElements) {
-                continue;
-            }
+    private function getTargetArticle(int $pageId, ContentTemplateArticleModel $templateArticle): ArticleModel
+    {
+        // Search by source ID
+        $targetArticle = ArticleModel::findOneBy(
+            ['pid = ?', 'content_template_source = ?'], 
+            [$pageId, $templateArticle->id]
+        );
 
-            $mappedElements = [];
+        // Search by alias and column
+        if (null === $targetArticle) {
+            $targetArticle = ArticleModel::findOneBy(
+                ['alias = ?', 'pid = ?', 'inColumn = ?'], 
+                [$templateArticle->alias, $pageId, $templateArticle->inColumn]
+            );
 
-            foreach ($templateElements as $templateElement) {
-                $targetElement = ContentModel::findOneBy(
-                    ['pid = ?', 'ptable = ?', 'content_template_source = ?'], 
-                    [$targetArticle->id, $targetArticle->getTable(), $templateElement->id]
-                );
-
-                if (null === $targetElement) {
-                    $targetCandidates = ContentModel::findBy(
-                        ['pid = ?', 'ptable = ?', 'type = ?'],
-                        [$targetArticle->id, $targetArticle->getTable(), $templateElement->type],
-                        ['order' => 'sorting ASC']
-                    );
-
-                    foreach ($targetCandidates ?? [] as $targetCandidate) {
-                        if (\in_array((int) $targetCandidate->id, $mappedElements, true)) {
-                            continue;
-                        }
-
-                        $targetElement = $targetCandidate;
-                        $targetElement->content_template_source = $templateElement->id;
-                        $targetElement->save();
-                        break;
-                    }
-                }
-
-                if (null === $targetElement) {
-                    $targetElement = new ContentModel();
-                    $row = $templateElement->row();
-                    unset($row[$targetElement->getPk()]);
-                    $targetElement->setRow($row);
-                    $targetElement->pid = $targetArticle->id;
-                    $targetElement->ptable = $targetArticle->getTable();
-                    $targetElement->save();
-                }
-
-                $mappedElements[] = (int) $targetElement->id;
-
-                // TODO: update settings
+            if (null !== $targetArticle) {
+                $targetArticle->content_template_source = $templateArticle->id;
+                $targetArticle->save();
             }
         }
+
+        // Create new article
+        if (null === $targetArticle) {
+            $targetArticle = new ArticleModel();
+            $row = $templateArticle->row();
+            unset($row[ArticleModel::getPk()]);
+            $targetArticle->setRow($row);
+            $targetArticle->pid = $pageId;
+            $targetArticle->content_template_source = $templateArticle->id;
+            $targetArticle->save();
+        }
+
+        return $targetArticle;
+    }
+
+    private function updateContentElements(ContentTemplateArticleModel $templateArticle, ArticleModel $targetArticle): void
+    {
+        // Get the content elements of the template
+        $templateElements = ContentModel::findBy(
+            ['pid = ?', 'ptable = ?'], 
+            [$templateArticle->id, $templateArticle->getTable()], 
+            ['order' => 'sorting ASC']
+        );
+
+        if (null === $templateElements) {
+            return;
+        }
+
+        $this->mappedElements = [];
+
+        foreach ($templateElements as $templateElement) {
+            $targetElement = $this->getTargetElement($targetArticle, $templateElement);
+
+            // TODO: update settings
+        }
+    }
+
+    private function getTargetElement(ArticleModel $targetArticle, ContentModel $templateElement): ContentModel
+    {
+        // Search by source ID
+        $targetElement = ContentModel::findOneBy(
+            ['pid = ?', 'ptable = ?', 'content_template_source = ?'], 
+            [$targetArticle->id, $targetArticle->getTable(), $templateElement->id]
+        );
+
+        // Search by type
+        if (null === $targetElement) {
+            $targetCandidates = ContentModel::findBy(
+                ['pid = ?', 'ptable = ?', 'type = ?'],
+                [$targetArticle->id, $targetArticle->getTable(), $templateElement->type],
+                ['order' => 'sorting ASC']
+            );
+
+            foreach ($targetCandidates ?? [] as $targetCandidate) {
+                if (\in_array((int) $targetCandidate->id, $this->mappedElements, true)) {
+                    continue;
+                }
+
+                $targetElement = $targetCandidate;
+                $targetElement->content_template_source = $templateElement->id;
+                $targetElement->save();
+                break;
+            }
+        }
+
+        // Create new content element
+        if (null === $targetElement) {
+            $targetElement = new ContentModel();
+            $row = $templateElement->row();
+            unset($row[$targetElement->getPk()]);
+            $targetElement->setRow($row);
+            $targetElement->pid = $targetArticle->id;
+            $targetElement->ptable = $targetArticle->getTable();
+            $targetElement->content_template_source = $templateElement->id;
+            $targetElement->save();
+        }
+
+        $this->mappedElements[] = (int) $targetElement->id;
+
+        return $targetElement;
     }
 }
