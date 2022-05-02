b0dd0863939a8a6ce46f4677acb9e93a9aca4a60
fritzmg <fmg@inspiredminds.at>
2022-05-02 16:27:30 +0100

    fix warning

--- contao/languages/en/tl_content_template_article.php
+++ contao/languages/en/tl_content_template_article.php
@@ -15,3 +15,5 @@ use Contao\System;
 System::loadLanguageFile('tl_article');
 
 $GLOBALS['TL_LANG']['tl_content_template_article'] = $GLOBALS['TL_LANG']['tl_article'];
+
+unset($GLOBALS['TL_LANG']['tl_content_template_article']['pasteafter']);
